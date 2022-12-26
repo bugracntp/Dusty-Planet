7662b053a0726422644ae4ce05ae2c46cd3a7c2f
furkan demirtas <furkandemirtas2134@hotmail.com>
2022-12-26 11:13:07 +0300

    front guncelleme

--- src/sliderMarsPhoto/slider.jsx
+++ src/sliderMarsPhoto/slider.jsx
@@ -3,48 +3,13 @@ import "./slider.css"
 
 export default function Slider() {
   return (
-    <div className='slider'>
-     <br /><br /> <h1>Alınan Fotoğraflar</h1>
-    <div id="slider1" className="csslider">
-        <input type="radio" name="slides" id="slides_1" checked />
-        <input type="radio" name="slides" id="slides_2"  />
-        <input type="radio" name="slides" id="slides_3" />
-        <input type="radio" name="slides" id="slides_4" />
-        <ul>
-            <li>
-              
-                <img src="http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/00999/opgs/edr/fcam/FRB_486178915EDR_F0481570FHAZ00206M_.JPG" />
-                
-            </li>
-            <li>
-                <img src="http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/00045/opgs/edr/fcam/FRA_401490757EDR_F0042092FHAZ00306M_.JPG" />
-                
-            </li>
-            <li>
-                <img src="http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/00321/opgs/edr/fcam/FRB_425982839EDR_F0060804FHAZ00327M_.JPG" />
-               
-            </li>
-            <li >
-                <img src=""></img>
-
-            </li>
-        </ul>
-        
-        <div className="arrows">
-            <label for="slides_1"></label>
-            <label for="slides_2"></label>
-            <label for="slides_3"></label>
-            <label for="slides_4"></label>
-        </div>
-        <div className="navigation">
-            <div>
-                <label for="slides_1"></label>
-                <label for="slides_2"></label>
-                <label for="slides_3"></label>
-                <label for="slides_4"></label>
-            </div>
-        </div>
+    <div class="Cerceve">
+    <div class="galeri">
+        <img src="http://mars.nasa.gov/mer/gallery/all/2/f/333/2F155927711EFF9946P1110R0M1-BR.JPG" alt=""/>
+        <img src="http://mars.nasa.gov/mer/gallery/all/2/f/190/2F143238746EFF7208P1203R0M1-BR.JPG" alt=""/>
+        <img src="http://mars.nasa.gov/mer/gallery/all/2/f/010/2F127262650EFF0214P1003L0M1-BR.JPG" alt=""/>
+        <img src="http://mars.nasa.gov/mer/gallery/all/1/r/012/1R129255699EFF0242P1211R0M1-BR.JPG" alt=""/>
     </div>
-    </div>
+   </div> 
   )
 }
